f6bb1656df86356e60dd60bb5550d9bf74c0876c
master game brito <Matheus Roque>
2026-09-05 14:36:47 -0300

    started project

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function search(){
+    nome = document.getElementById("searchname").value
+    if(nome ==""){
+        console.log("Sessao de nomes vazia");
+    }
+    console.log("iniciando busca por "+nome)
+}
---
+++ python/main.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+
+print("iniciando programa")
+nome = input("insira o nome do influenciador")
+print(nome)
