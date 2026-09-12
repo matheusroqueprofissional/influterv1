b152affcf979902bf6fcd994b6f9b17fcccb5341
Matheus Roque <matheus.roqueprofissional@gmail.com>
2026-09-11 23:02:38 -0300

    done database and backend for sign

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask import request
 
 import mysql.connector
@@ -6,39 +6,56 @@ import mysql.connector
 from flask import Flask, render_template
 from pathlib import Path
 
-# Caminho da pasta v01
 ROOT = Path(__file__).resolve().parents[2]
+app = Flask(__name__, template_folder=str(ROOT / "html"))
 
-conexao = mysql.connector.connect(
+# Caminho da pasta v01
+
+def conectar_banco():
+    return mysql.connector.connect(
     host="127.0.0.1",
     user="root",
     password="#Luis454049",
     database="users"
-)
+    )
 
-cursor = conexao.cursor()
+@app.route("/register",methods=["GET", "POST"])
+def registro():
+    conectar_banco()
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
 
-cursor.execute("SELECT * FROM user")
-
-resultados = cursor.fetchall()
-
-for usuario in resultados:
-    print(usuario)
-
-cursor.close()
-conexao.close()
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
 
 
-app = Flask(__name__, template_folder=str(ROOT / "html"))
+        valores = (
+            username,
+            email,
+            password
+        )
 
-@app.route("/")
-def test():
-    print("working")
-    return "<p>is working</p>"
+        sql = """INSERT INTO user (username, email, password) VALUES (%s, %s, %s)"""
 
-@app.route("/profile/<username>")
-def profile(username):
-    return render_template('../html/profile.html',username="matheus")
+        try:
+            cursor.execute(sql, valores)
+            conexao.commit()
+
+        except mysql.connector.IntegrityError:
+            cursor.close()
+            conexao.close()
+
+            return "Username ou email já cadastrado."
+
+        cursor.close()
+        conexao.close()
+
+        return redirect(url_for("http://localhost:5500/v01/html/", username=username))
+    return render_template("http://localhost:5500/v01/html/")
+
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
